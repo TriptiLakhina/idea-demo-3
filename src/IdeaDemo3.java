58a5bc9c31ec5802d7749c305ac678f0c04435ca
tripti <triptilakhina9286@gmail.com>
2023-03-12 16:31:03 +0000

    commit idea demo 3

--- src/IdeaDemo3.java
+++ src/IdeaDemo3.java
@@ -1,5 +1,8 @@
 public class IdeaDemo3 {
     public static void main(String[]args){
         System.out.println("ideaDemo3");
+        System.out.println("Hello commit");
+        System.out.println("commit -3");
     }
+
 }
